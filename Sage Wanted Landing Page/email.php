d6ee02fb5991c6521ff505b830d9fe5e7b08a831
storib <adrian.conley@gmail.com>
2016-02-28 13:36:41 -0800

    Added in Analytics (Google) and a Thank You for signing up page

--- Sage Wanted Landing Page/email.php
+++ Sage Wanted Landing Page/email.php
@@ -9,32 +9,30 @@ $sender_subjects = filter_var($_POST["subjects"], FILTER_SANITIZE_STRING); //cap
 $sender_message = filter_var($_POST["otherMessage"], FILTER_SANTITIZE_STRING);
 
 //Create the SMTP configuration
-$to = array('adrian.conley@gmail.com' => 'Adrian Conley'); //recepient
-$from = array('noreply@sagewanted.com' => 'SageWanted'); //from email using site domain.
-$subject = 'New Sign Up!'; //email subject line
-$text = '$sender_name signed up on SageWanted.com. Their email address is $sender_email
-        and wanted to register as $sender_sageRegistration with the subjects $sender_subjects. They also would like to tell 
-        $sender_message';
-
-$transport = Swift_SmtpTransport::newInstance("smtp.sagewanted.net");
-$transport->setUsername("sagewantedcom");
-$transport->setPassword("fakepassword");
+$transport = Swift_SmtpTransport::newInstance("smtp.gmail.net", 465, "ssl")
+	->setUsername("adrian@sagewanted.com")
+	->setPassword("xxx");
 
 //create mailer
-$swift = Swift_Mailer::newInstance($transport);
+$mailer = Swift_Mailer::newInstance($transport);
 
-$message = new Swift_Message($subject);
-$message->setFrom($from);
-$message->setBody($text);
+$message = Swift_Message::newInstance('New Sign Up!')
+	->setFrom(array('noreply@sagewanted.com' => 'SageWanted'))
+	->setTo(array('adrian@sagewanted.com' => 'Adrian Conley'))
+	->setBody('$sender_name signed up on SageWanted.com. Their email address is $sender_email
+        and wanted to register as $sender_sageRegistration with the subjects $sender_subjects. They also would like to tell 
+        $sender_message');
 
-    
 //send message
 $result = $mailer->send($message);
 
-if ($result)
+echo($result);
+/*if($recipients = $mailer->send($message, $failures))
 {
-    header('location: thank-you.html');
-}
-
+	echo 'Message successfully sent!';
+} else {
+	echo "There was an error sending your message:\n";
+	print_r($failures);
+}*/
 ?>
 
