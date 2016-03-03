459a4564e0fad438471f97fca46801720b536101
storib <adrian.conley@gmail.com>
2016-03-02 21:53:46 -0800

    Adi’s wish is my commit…command? Boom.

--- unit_6/email.php
+++ unit_6/email.php
@@ -1,10 +1,10 @@
 <?php
 require_once 'lib/swift_required.php';
 
-//try{//grab post data
+try{//grab post data
 $sender_name = filter_var($_POST["nameInput"], FILTER_SANITIZE_STRING); //capture sender name
 $sender_email = filter_var($_POST["emailInput"], FILTER_SANITIZE_STRING); //capture sender email
-$sender_sageRegistration = filter_var($_Post["interest"], FILTER_SANITIZE_STRING); //capture interest 
+$sender_interest = filter_var($_POST["interest"], FILTER_SANITIZE_STRING); //capture interest 
 $sender_subjects = filter_var($_POST["subjects"], FILTER_SANITIZE_STRING); //capture message
 $sender_location = filter_var($_POST["location"], FILTER_SANITIZE_STRING);
 $sender_message = filter_var($_POST["s_message"], FILTER_SANITIZE_STRING);
@@ -21,18 +21,19 @@ $message = Swift_Message::newInstance('New Sign Up!')
 	->setFrom(array('noreply@sagewanted.com' => 'SageWanted'))
 	->setTo(array('adrian@sagewanted.com' => 'Adrian Conley'))
 	->setBody($sender_name ." " . "signed up on SageWanted.com. Their email address is" ." ". $sender_email ." ".
-        "and wanted to register as a". $sender_sageRegistration ." with the subjects ". $sender_subjects . 
-        "They also would like to tell ". $sender_message . "and are located in" . $sender_location);
+        "and wanted to ". $sender_interest ." with the subjects ". $sender_subjects . ". " 
+        "They also would like to tell ". $sender_message . "they are located in" . " " . $sender_location);
 
 //send message
 $result = $mailer->send($message);
-//print_r($result);
-if ($result)
+print_r($result);
+} catch(Exception $e){
+	echo $e->getMessage();
+}
+
+/*if ($result)
 {
     header('location: thankyou.html');
-}
-//} catch(Exception $e){
-//	echo $e->getMessage();
-//}
+}*/
 ?>
 
